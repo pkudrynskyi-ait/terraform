5eb5065ac88ee6220f30194aa57fa390ccaf3efa
pkudrynskyi-ait <pavlo.kudrynskyi@automat-it.com>
2026-04-22 16:21:34 +0300

    infra: Create S3 bucket pavlo-ait-feature4 in Frankfurt region

---
+++ development/eu-central-1/s3/pavlo-ait-feature4/main.tf
@@ -0,0 +1,21 @@
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.0"
+
+  bucket = "pavlo-ait-feature4"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = true
+  }
+
+  tags = {
+    Environment = "development"
+    ManagedBy   = "terraform"
+    Project     = "poc"
+  }
+}
